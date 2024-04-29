81190af01a49b315f6408a73d36bc5bdd81b6e15
M7RB <94798114+M7RB@users.noreply.github.com>
2024-04-29 13:49:19 +0000

    Update Password

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: 'password',
+    interstellar: 'Password',
+    M7RB: 'M7RB',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
